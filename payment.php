c2ab68b3d1d77306fba28afa9c59060964c5cd02
73kn1k <80842155+73kn1k@users.noreply.github.com>
2024-08-08 11:10:53 -0400

    Create payment.php

---
+++ payment.php
@@ -0,0 +1,50 @@
+<?php
+
+  // Stripe documentation can be found at https://docs.stripe.com/api?lang=php
+  require_once('include/vendor/stripe/init.php');
+
+  \Stripe\Stripe::setApiKey($stripe['secret_key']);
+  if (isset($_GET['e']) && $_GET['e'] == 1 && isset($_POST['stripeToken']) && !empty($_POST['stripeToken'])) {
+    
+    // Stripe payment process
+    
+    $stripeToken  = $_POST['stripeToken'];
+    $email        = $_POST['stripeEmail'];
+    
+    $in_transaction = true;
+
+    try {
+      
+      $daStripeP = number_format(( item_price * $_POST['stripeQuantity']), 2, "", "");
+      
+      $customer = \Stripe\Customer::create([
+        'email' => $email,
+        'source'  => $stripeToken,
+      ]);
+
+      $charge = \Stripe\Charge::create([
+        'customer' => $customer->id,
+        'amount'   => $daStripeP,
+        'currency' => 'USD',
+        'description' => $item->nombre,
+      ]);
+
+      $kdaNtw = ($isTestnet == true) ? "testnet04" : "mainnet01";
+      $chainId = ($isTestnet == true) ? "1" : "8";
+
+      $_url = "http://127.0.0.1:3000/createToken?ntw=$kdaNtw&chainId=$chainId&collection_id=".urlencode( stored_collection_id )."&customer_pubK=". userSessionKDAwallet ."&store_pubK=". olimpiaKDAwallet ."&uri=".urlencode($_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'].substr($newFileNameJSON,1));
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL, $_url);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+      $response = curl_exec($ch);
+      $response_data = json_decode($response, true);
+
+      $tx = $response_data['data']['tokenId'];
+      $transaction_tx = $response_data['data']['reqK'];
+
+      . . .
+        
+         db data stuff
+
+          EOF
