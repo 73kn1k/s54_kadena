6b426d5067fc75fb937f55fe6d0c511ffb9f34c1
73kn1k <80842155+73kn1k@users.noreply.github.com>
2024-08-08 11:19:53 -0400

    Update payment.php

--- payment.php
+++ payment.php
@@ -3,11 +3,13 @@
   // Stripe documentation can be found at https://docs.stripe.com/api?lang=php
   require_once('include/vendor/stripe/init.php');
 
-  \Stripe\Stripe::setApiKey($stripe['secret_key']);
+  // The $_POST['stripeToken'] is received from the Stripe component when a payment is issued. And here it is handled
+  // All validation code can be found in the Stripe documentation and for ease of reading is omitted in this document.
+
+  \Stripe\Stripe::setApiKey( "SecretKey" );
   if (isset($_GET['e']) && $_GET['e'] == 1 && isset($_POST['stripeToken']) && !empty($_POST['stripeToken'])) {
     
     // Stripe payment process
-    
     $stripeToken  = $_POST['stripeToken'];
     $email        = $_POST['stripeEmail'];
     
@@ -26,13 +28,13 @@
         'customer' => $customer->id,
         'amount'   => $daStripeP,
         'currency' => 'USD',
-        'description' => $item->nombre,
+        'description' => "itemNameDemo" ,
       ]);
 
       $kdaNtw = ($isTestnet == true) ? "testnet04" : "mainnet01";
       $chainId = ($isTestnet == true) ? "1" : "8";
 
-      $_url = "http://127.0.0.1:3000/createToken?ntw=$kdaNtw&chainId=$chainId&collection_id=".urlencode( stored_collection_id )."&customer_pubK=". userSessionKDAwallet ."&store_pubK=". olimpiaKDAwallet ."&uri=".urlencode($_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'].substr($newFileNameJSON,1));
+      $_url = "http://127.0.0.1:3000/createToken?ntw=$kdaNtw&chainId=$chainId&collection_id=".urlencode( stored_collection_id )."&customer_pubK=". userSessionKDAwallet ."&store_pubK=". olimpiaKDAwallet ."&uri=".urlencode($_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST']. newJsonName);
       $ch = curl_init();
       curl_setopt($ch, CURLOPT_URL, $_url);
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
